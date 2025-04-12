7f8f6ef3165af97e6e777f85ba5d69814b91f42c
Grihith-Reddy <grihithreddy9870@gmail.com>
2025-04-12 23:35:18 +0530

    Initial commit

---
+++ src/test/java/io/github/grihithreddy/onlinecoursemanagement/OnlineCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.grihithreddy.onlinecoursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
