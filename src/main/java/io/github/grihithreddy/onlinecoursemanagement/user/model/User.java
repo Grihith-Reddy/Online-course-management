6d00fd7ca57fe85e6bfbe36d9eac17c5be0ad408
MdSalman0007 <mdsalman62045@gmail.com>
2025-04-14 01:05:48 +0530

    user model created

---
+++ src/main/java/io/github/grihithreddy/onlinecoursemanagement/user/model/User.java
@@ -0,0 +1,111 @@
+package io.github.grihithreddy.onlinecoursemanagement.user.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank()
+    @Size(min = 3, max = 25 , message = "Name should be between 3-25 characters")
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank()
+    @Size(min = 5,message = "email shouldnot less than r characters" )
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank()
+    @Size(min = 8, max = 20, message = "password greater than 8 but less than 20")
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank(message = "please provide ph no")
+    private String phone;
+
+    public User() {
+    }
+
+    public User(int id, String name, String email, String password, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id
+                && Objects.equals(name, user.name)
+                && Objects.equals(email, user.email)
+                && Objects.equals(password, user.password)
+                && Objects.equals(phone, user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password, phone);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
