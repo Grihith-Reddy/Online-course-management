211cfa32526af6bbc43b1e09f5749111cc33a688
Grihith-Reddy <grihithreddy9870@gmail.com>
2025-04-13 21:49:54 +0530

    Created the course validation exception handling

--- src/main/java/io/github/grihithreddy/onlinecoursemanagement/course/exceptions/CourseValidationException.java
+++ src/main/java/io/github/grihithreddy/onlinecoursemanagement/course/exceptions/CourseValidationException.java
@@ -1,6 +1,16 @@
 package io.github.grihithreddy.onlinecoursemanagement.course.exceptions;
 
+/**
+ * Exception thrown when a validation error occurs during course processing.
+ * This class extends {@link RuntimeException} to indicate that it is an unchecked exception.
+ */
 public class CourseValidationException extends RuntimeException {
+
+    /**
+     * Constructor to create a new CourseValidationException with a custom message.
+     *
+     * @param message the detail message about the validation error
+     */
     public CourseValidationException(String message) {
         super(message);
     }
