fc0a183db6e42d77683e2d25409d41560d914e06
Grihith-Reddy <grihithreddy9870@gmail.com>
2025-04-13 21:49:27 +0530

    Created the course not found exception handling

--- src/main/java/io/github/grihithreddy/onlinecoursemanagement/course/exceptions/CourseNotFoundException.java
+++ src/main/java/io/github/grihithreddy/onlinecoursemanagement/course/exceptions/CourseNotFoundException.java
@@ -1,6 +1,16 @@
 package io.github.grihithreddy.onlinecoursemanagement.course.exceptions;
 
+/**
+ * Exception thrown when a course is not found in the system.
+ * This class extends {@link RuntimeException} to indicate that it is an unchecked exception.
+ */
 public class CourseNotFoundException extends RuntimeException {
+
+  /**
+   * Constructor to create a new CourseNotFoundException with a custom message.
+   *
+   * @param message the detail message about the course not found
+   */
   public CourseNotFoundException(String message) {
     super(message);
   }
